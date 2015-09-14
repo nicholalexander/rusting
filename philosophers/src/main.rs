a32b49011a3feb1384fca14f5e322c320f775f81
nichol alexander <nichol.alexander@gmail.com>
2015-09-13 22:03:40 -0400

    add philosophers, eating, add thread

--- philosophers/src/main.rs
+++ philosophers/src/main.rs
@@ -1,3 +1,34 @@
+use std::thread;
+
+struct Philosopher {
+  name: String,
+
+}
+
+impl Philosopher {
+  fn new(name: &str) -> Philosopher {
+    Philosopher {
+      name: name.to_string(),
+    }
+  }
+
+  fn eat(&self) {
+    println!("{} is eating...", self.name);
+    thread::sleep_ms(1000);
+    println!("{} is done eating.", self.name);
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  let philosophers = vec! [
+    Philosopher::new("Judith Butler"),
+    Philosopher::new("Giles Deleuze"),
+    Philosopher::new("Karl Marx"),
+    Philosopher::new("Emma Goldman"),
+    Philosopher::new("Michel Foucault"),
+  ];
+
+  for p in &philosophers {
+    p.eat();
+  } 
 }
